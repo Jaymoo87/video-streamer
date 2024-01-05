4346e22075d57e4ec90e7311bb0f438c440fb2d2
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2024-01-04 20:10:24 -0600

    hydration error resolved, must render consistent state between server and client.

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -7,14 +7,29 @@ interface WrapperProps {
   children: React.ReactNode;
 }
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended";
 
 export const Wrapper = ({ children }: WrapperProps) => {
+  const [isClient, setIsClient] = useState(false);
   const { collapsed } = useSideBar((state) => state);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient)
+    return (
+      <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border=[#2d2e35] z-50">
+        <ToggleSkeleton />
+        <RecommendedSkeleton />
+      </aside>
+    );
   return (
     <aside
       className={cn(
-        "fixed left-0 flex flex-col w-60 h-full bg-background border-r border=[#2d2e35] z-50",
+        "fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border=[#2d2e35] z-50",
         collapsed && "w-[70px]"
       )}
     >
