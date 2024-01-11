7324c52494300c9aacf5691209c2e3e273107802
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2024-01-11 14:30:37 -0600

    fine tuned the follow flow. followed users dont show up on recommended list

--- lib/follow-service.ts
+++ lib/follow-service.ts
@@ -28,6 +28,24 @@ export const isFollowingUser = async (id: string) => {
   }
 };
 
+export const getFollowedUsers = async () => {
+  try {
+    const self = await getSelf();
+
+    const followedUsers = await db.follow.findMany({
+      where: {
+        followerId: self.id,
+      },
+      include: {
+        following: true,
+      },
+    });
+    return followedUsers;
+  } catch (error) {
+    return [];
+  }
+};
+
 export const followUser = async (id: string) => {
   const self = await getSelf();
 
