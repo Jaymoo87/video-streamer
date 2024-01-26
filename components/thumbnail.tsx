22ea245c22b3ebfafd0b20d93f41e62bcd04ccb7
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2024-01-25 21:25:47 -0600

    fixed thumbails and results cards UI. moved livebadge to Thumbnail comp

--- components/thumbnail.tsx
+++ components/thumbnail.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { UserAvatar } from "./useravatar";
 import Image from "next/image";
 import { Skeleton } from "./ui/skeleton";
+import { LiveBadge } from "./livebadge";
 
 interface ThumbnailProps {
   src: string | null;
@@ -15,7 +16,7 @@ export const Thumbnail = ({ src, fallback, isLive, username }: ThumbnailProps) =
 
   if (!src) {
     content = (
-      <div className="bg-background flex flex-col items-center justify-center gap-y-4 h-full w-full transition-transform group-hover:translate-x-2 group-hover:translate-y-1 rounded-md">
+      <div className="bg-background flex flex-col items-center justify-center gap-y-4 h-full w-full transition-transform group-hover:translate-x-2 group-hover:-translate-y-1 rounded-md">
         <UserAvatar size={"lg"} showBadge username={username} imageUrl={fallback} isLive={isLive} />
       </div>
     );
@@ -25,15 +26,20 @@ export const Thumbnail = ({ src, fallback, isLive, username }: ThumbnailProps) =
         src={src}
         fill
         alt="thumbnail"
-        className="object-cover transition-transform group-hover:translate-x-2 group-hover:translate-y-2 rouned-md"
+        className="object-cover transition-transform group-hover:translate-x-2 group-hover:-translate-y-2 rouned-md"
       />
     );
   }
 
   return (
-    <div className="group apsect-video relative rounded-md cursor-pointer">
+    <div className="group aspect-video relative rounded-md cursor-pointer">
       <div className="rounded-md absolute inset-0 bg-blue-600 opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center" />
       {content}
+      {isLive && src && (
+        <div className="absolute top-2 left-2 group-hover:translate-x-2 group-hover:-translate-y-2 transition-transform">
+          <LiveBadge />
+        </div>
+      )}
     </div>
   );
 };
